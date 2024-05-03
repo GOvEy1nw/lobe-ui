c2ecbd5569e87f95972def1242e9e1631de5d530
GOvEy1nw <45126527+GOvEy1nw@users.noreply.github.com>
2024-05-03 16:35:52 +0100

    update

--- src/Logo/style.ts
+++ src/Logo/style.ts
@@ -1,19 +1,19 @@
 import { createStyles } from 'antd-style';
 
 export const LOGO_3D = {
-  path: './assets/rais-logo.svg', // Relative path to the logo file
+  path: '/assets/rais-logo.svg', // Relative path to the logo file
   pkg: '', // No external package
   version: '', // No version
 };
 
 export const LOGO_FLAT = {
-  path: './assets/rais-icon.svg', // Relative path to the logo file
+  path: '/assets/rais-icon.svg', // Relative path to the logo file
   pkg: '', // No external package
   version: '', // No version
 };
 
 export const LOGO_TEXT = {
-  path: './assets/rais-logotext.svg', // Relative path to the logo file
+  path: '/assets/rais-logotext.svg', // Relative path to the logo file
   pkg: '', // No external package
   version: '', // No version
 };
